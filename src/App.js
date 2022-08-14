f049b77ed35667cd8ffd0456bcb79216dd0ee52b
Juan Saldana <saldanaj97@gmail.com>
2022-08-14 16:13:38 -0500

    Added header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import { Box } from "@mui/material";
 import { Paper } from "@mui/material";
+import Appheader from "./components/appheader";
+import Container from "@mui/material/Container";
 
 function App() {
   return (
     <div className='App'>
-      <Paper className='background' elevation={5} sx={{ m: "20% auto", p: "3px", backgroundColor: "#b4213a" }}>
-        <Box className='main-container'>Hey</Box>
+      <Paper className='background' elevation={8} sx={{ display: "flex", justifyContent: "flex-end", m: "15% auto", backgroundColor: "#b4213a", borderRadius: "15px" }}>
+        <Container className='main-container' sx={{ margin: "3px", padding: "10px", width: "75%" }}>
+          <Appheader />
+        </Container>
       </Paper>
     </div>
   );
---
+++ src/components/appheader.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+
+const AppHeader = () => {
+  return (
+    <AppBar position='static' elevation={8} sx={{ flexDirection: "row", backgroundColor: "white", borderRadius: "15px", height: "10%" }}>
+      <Container maxWidth='xl' sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", height: "100%" }}>
+        <Container className='title' sx={{ display: "flex", alignItems: "center", height: "100%" }}>
+          <Typography variant='h6' sx={{ mr: 2, display: { xs: "none", md: "flex" }, fontWeight: 700, color: "black", textDecoration: "none" }}>
+            Race Planner
+          </Typography>
+        </Container>
+
+        <IconButton className='avatar-button' sx={{ display: "flex", alignContent: "flex-end" }}>
+          <Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
+        </IconButton>
+      </Container>
+    </AppBar>
+  );
+};
+export default AppHeader;
