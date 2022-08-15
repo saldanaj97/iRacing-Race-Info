0d2499dbbb25431a2bdebc6f1991a0941fd82289
Juan Saldana <saldanaj97@gmail.com>
2022-08-14 21:12:43 -0500

    Adjusted app size and margins

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 function App() {
   return (
     <div className='App'>
-      <Paper className='background' elevation={8} sx={{ display: "flex", justifyContent: "flex-end", m: "15% auto", backgroundColor: "#2b2d42", borderRadius: "15px" }}>
+      <Paper className='background' elevation={8} sx={{ display: "flex", justifyContent: "flex-end", m: "5% auto", backgroundColor: "#2b2d42", borderRadius: "15px" }}>
         <Sidebar />
         <Container className='main-container' sx={{ margin: "3px", padding: "10px", width: "75%", backgroundColor: "white", borderRadius: "0 15px 15px 0" }}>
           <Appheader />
