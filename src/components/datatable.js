4c08d8f20355307ebf83503da6e90618eec7943a
Juan Saldana <saldanaj97@gmail.com>
2022-08-17 17:25:45 -0500

    Added in car data

--- src/components/datatable.js
+++ src/components/datatable.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Paper } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import RaceData from "../data/current-season-schedules.json";
+import CarData from "../data/car-data.json";
 
 /* Column headers for the table */
 const columns = [
@@ -11,32 +12,37 @@ const columns = [
     field: "seriesName",
     headerName: "Series",
     width: 250,
-    editable: true,
+    editable: false,
+    headerAlign: "center",
   },
   {
     field: "cars",
     headerName: "Cars",
-    width: 100,
-    editable: true,
+    width: 200,
+    editable: false,
+    headerAlign: "center",
   },
   {
     field: "track",
     headerName: "Track",
-    width: 250,
-    editable: true,
+    width: 275,
+    editable: false,
+    headerAlign: "center",
   },
   {
     field: "duration",
     headerName: "Duration",
     width: 80,
     sortable: false,
-    editable: true,
+    editable: false,
+    headerAlign: "center",
   },
   {
     field: "setup",
     headerName: "Fixed",
     description: "This column has a value getter and is not sortable.",
-    width: 80,
+    width: 55,
+    headerAlign: "center",
   },
 ];
 
@@ -48,6 +54,39 @@ const licenses = {
   4: "B",
   5: "A",
 };
+
+// This will be used to the code for a category to the real category for display
+const categories = {
+  road: "Road",
+  oval: "Oval",
+  dirt_oval: "Dirt Oval",
+  dirt_road: "RX",
+};
+
+// This will be used to convert a boolean value to a proper value to be displayed
+const fixedSetup = {
+  true: "Yes",
+  false: "No",
+};
+
+/* Function that will gather all the data for the cars 
+  Parameters: N/A
+  Returns: an object of cars with their ids and the names 
+*/
+const getCarData = () => {
+  const carsIdsAndNames = [];
+
+  // Get the data from the CarData json function
+  Object.values(CarData).map((car) => {
+    let carId = car.id;
+    let carName = car.shortName;
+    carsIdsAndNames.push({ carId, carName });
+  });
+  return carsIdsAndNames;
+};
+
+// Get the car ids and names
+const carNames = getCarData();
 
 /* Function that will take in the number of minutes and convert to hours and min
   Parameters: min - the number of min
@@ -68,16 +107,26 @@ const timeConvert = (min) => {
 */
 const getSeriesData = (weekNum) => {
   const rows = [];
+
+  // Get the data from the racedata json file
   Object.values(RaceData).map((series) => {
     let id = series.series_id;
     let license = licenses[series.license_group];
-    let category = series.schedule[weekNum].track.category;
+    let category = categories[series.schedule[weekNum].track.category];
     let seriesName = series.series_name;
-    let cars = series.car_class_ids;
+    let carIds = series.car_class_ids;
     let track = series.schedule[weekNum].track.track_name;
     let duration = series.schedule[weekNum].race_time_limit !== null ? timeConvert(series.schedule[weekNum].race_time_limit) : series.schedule[weekNum].race_lap_limit + " L";
-    let setup = series.fixed_setup;
+    let setup = fixedSetup[series.fixed_setup];
 
+    // Get each name for the cars by using the ID to find a match in the carNames object
+    let cars = [];
+    carIds.forEach((car) => {
+      let names = carNames.find((vehicle) => vehicle.carId === car);
+      cars.push(names.carName);
+    });
+
+    // Add the data to the rows array
     rows.push({ id, license, category, seriesName, cars, track, duration, setup });
   });
   return rows;
@@ -90,7 +139,7 @@ export default function Data() {
   return (
     <Paper elevation={8} sx={{ borderRadius: "15px", width: "95%" }}>
       <Box sx={{ width: "100%" }}>
-        <DataGrid rows={rows} columns={columns} pageSize={10} rowsPerPageOptions={[10]} autoPageSize='true' autoHeight='true' disableSelectionOnClick experimentalFeatures={{ newEditingApi: true }} />
+        <DataGrid rows={rows} columns={columns} pageSize={10} rowsPerPageOptions={[10]} autoPageSize='true' autoHeight='true' disableSelectionOnClick experimentalFeatures={{ newEditingApi: true }} word />
       </Box>
     </Paper>
   );
