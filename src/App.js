77ad2873a04d1f81a97ae89db480d711b7cb647b
Juan Saldana <saldanaj97@gmail.com>
2022-08-23 18:23:11 -0500

    Added new routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { WeekProvider } from "./contexts/WeekContext";
 import RaceDashboard from "./pages/RaceDashboard";
+import Cars from "./pages/Cars";
+import Tracks from "./pages/Tracks";
+import Series from "./pages/Series";
 import "./App.css";
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' exact element={<RaceDashboard />} />
+          <Route path='/cars' exact element={<Cars />} />
+          <Route path='/tracks' exact element={<Tracks />} />
+          <Route path='/series' exact element={<Series />} />
         </Routes>
       </Router>
     </WeekProvider>
