07b691b7352a06b165f376ef2f641cc00e67763f
Juan Saldana <saldanaj97@gmail.com>
2022-08-25 19:22:12 -0500

    Create user and get all users implemented with mongoose

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv").config({ path: "./.env" });
 
 // Mongo URI
 const url = process.env.MONGODB_URI;
-console.log(url);
 
 mongoose.connect(url, {
   useNewUrlParser: true,
---
+++ server/models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const { uuid } = require("uuidv4");
+
+// Table schema for a user
+const userSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: () => uuid().replace(/\-/g, ""),
+    },
+    username: {
+      type: String,
+      unique: true,
+    },
+    password: String,
+    series: Array,
+    cars: Array,
+    tracks: Array,
+  },
+  { collection: "users" }
+);
+
+// Function to create a user in the users collection
+userSchema.statics.createUser = async function (username, password) {
+  try {
+    const user = await this.create({
+      username,
+      password,
+    });
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Function to get all users in the collection
+userSchema.statics.getAllUsers = async function () {
+  try {
+    const allUsers = await this.find();
+    return allUsers;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
