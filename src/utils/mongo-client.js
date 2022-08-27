252541d355cfc5b64e2749236c3e030c6c31f732
Juan Saldana <saldanaj97@gmail.com>
2022-08-26 20:05:17 -0500

    Requests are sent to atlas but not successfully logging in

--- src/utils/mongo-client.js
+++ src/utils/mongo-client.js
@@ -3,4 +3,5 @@ import * as Realm from "realm-web";
 const REALM_APP_ID = process.env.REACT_APP_REALM_APP_ID;
 
 export const app = new Realm.App({ id: REALM_APP_ID });
+
 export const credentials = Realm.Credentials.anonymous();
