acf470082eacfd7e9ea8378f4ad10703447a7906
Juan Saldana <saldanaj97@gmail.com>
2022-08-31 20:44:05 -0500

    Added function to retrieve users owned cars on load

--- server/controllers/UserContent.js
+++ server/controllers/UserContent.js
@@ -1,13 +1,23 @@
 const UserModel = require("../models/User");
 
-const getOwnedCars = async (req, res) => {
+const getUsersOwnedCars = async (req, res) => {
   try {
-    const { user, cars } = req.body;
-    const updatedOwnedCars = UserModel.updateOwnedCars(user, cars);
-    return res.status(200).json({ success: true, message: "Users owned cars updated. ", updatedOwnedCars });
+    const { user } = req.body;
+    const ownedCars = UserModel.getOwnedCars(user);
+    return res.status(200).json({ success: true, ownedCars });
   } catch (error) {
     return res.status(500).json({ success: false, error: error });
   }
 };
 
-module.exports = { getOwnedCars };
+const updateOwnedCars = async (req, res) => {
+  try {
+    const { user, cars } = req.body;
+    const ownedCars = UserModel.updateOwnedCars(user, cars);
+    return res.status(200).json({ success: true, message: "Users owned cars updated. ", ownedCars });
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error });
+  }
+};
+
+module.exports = { getUsersOwnedCars, updateOwnedCars };
