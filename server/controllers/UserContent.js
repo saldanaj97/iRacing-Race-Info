dffc28dc55435c3d798edf2efe59d79af2d9a4cc
Juan Saldana <saldanaj97@gmail.com>
2022-08-31 19:12:04 -0500

    Atlas now updates the owned cars field according to the userId of the person logged in.

--- server/controllers/UserContent.js
+++ server/controllers/UserContent.js
@@ -3,10 +3,8 @@ const UserModel = require("../models/User");
 const getOwnedCars = async (req, res) => {
   try {
     const { user, cars } = req.body;
-    console.log(cars);
-    const { email } = user.profile.data;
-    //const updatedOwnedCars = UserModel.updateOwnedCars(email, cars);
-    return res.status(200).json({ success: true, message: "Users owned cars updated. " });
+    const updatedOwnedCars = UserModel.updateOwnedCars(user, cars);
+    return res.status(200).json({ success: true, message: "Users owned cars updated. ", updatedOwnedCars });
   } catch (error) {
     return res.status(500).json({ success: false, error: error });
   }
