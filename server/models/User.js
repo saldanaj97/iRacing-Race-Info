81c0817e7adb3b3c0506cd0f6bdce368755058b6
Juan Saldana <saldanaj97@gmail.com>
2022-08-31 19:12:04 -0500

    Atlas now updates the owned cars field according to the userId of the person logged in.

--- server/models/User.js
+++ server/models/User.js
@@ -23,44 +23,10 @@ const userSchema = new mongoose.Schema(
 
 const db = mongoose.connection.useDb("iRacingWeeklyDB");
 
-// Function to create a user in the users collection
-userSchema.statics.createUser = async function (username, password) {
+userSchema.statics.updateOwnedCars = async function (userObj, cars) {
   try {
-    const user = await this.create({
-      username,
-      password,
-    });
-    return user;
-  } catch (error) {
-    throw error;
-  }
-};
-
-// Function to get all users in the collection
-userSchema.statics.getAllUsers = async function () {
-  try {
-    const allUsers = await this.find();
-    return allUsers;
-  } catch (error) {
-    throw error;
-  }
-};
-
-// Function to get a user by username
-userSchema.statics.getUserByUsername = async function (username) {
-  try {
-    const user = await this.findOne({ username: username });
-    return user;
-  } catch (error) {
-    throw error;
-  }
-};
-
-userSchema.statics.updateOwnedCars = async function (email, cars) {
-  try {
-    const user = this.findOne({ email: email });
+    const user = this.findOne({ "username.user.id": userObj.id });
     const updated = await this.updateOne(user, { $set: { cars: cars } });
-    console.log(updated);
     return updated;
   } catch (error) {
     throw error;
