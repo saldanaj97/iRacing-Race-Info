3fcd383270cab249c2abe89087d178841b5c57de
Juan Saldana <saldanaj97@gmail.com>
2022-09-01 20:08:26 -0500

    Added function for grabbing owned tracks and updating users tracks

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -16,7 +16,28 @@ const updateOwnedCars = async (req, res) => {
   try {
     const { user, cars } = req.body;
     const ownedCars = UserModel.updateOwnedCars(user, cars);
-    return res.status(200).json({ success: true, message: "Users owned cars updated. ", ownedCars });
+    return res.status(200).json({ success: true, message: "Owned cars updated. ", ownedCars });
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error });
+  }
+};
+
+// Function that will gather the users owned tracks
+const getOwnedTracks = async (req, res) => {
+  try {
+    const { user } = req.body;
+    const ownedTracks = await UserModel.getOwnedTracks(user);
+    return res.status(200).json({ success: true, ownedTracks });
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error });
+  }
+};
+// Function to update the users owned tracks
+const updateOwnedTracks = async (req, res) => {
+  try {
+    const { user, tracks } = req.body;
+    const ownedTracks = UserModel.updateOwnedTracks(user, tracks);
+    return res.status(200).json({ success: true, message: "Owned tracks updated. ", ownedTracks });
   } catch (error) {
     return res.status(500).json({ success: false, error: error });
   }
