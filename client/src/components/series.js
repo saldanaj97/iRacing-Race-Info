a184157e9f10aa2aedd5960a3dbb6250a34ed9f4
Juan Saldana <saldanaj97@gmail.com>
2022-09-03 12:25:03 -0500

    Series filter now working properly

--- client/src/components/series.js
+++ client/src/components/series.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Axios from "axios";
 import { Box, Button, Checkbox, Paper, FormGroup, FormControlLabel } from "@mui/material";
 import SeriesData from "../data/series.json";
@@ -8,24 +8,23 @@ import { getUserFavoritedSeries } from "../services/Services";
 export default function FavoriteSeries() {
   // Global user data
   const { user } = useContext(UserContext);
-
-  // Favorite series map which maps a track to a bool whether the user owns the track or not
-  const favoriteSeries = new Map();
+  const [usersFavoriteSeries, setUsersFavoriteSeries] = useState(new Map());
 
   // The different types of races cars will be a part of
   const types = ["road", "oval", "dirt_oval", "dirt_road"];
   const typesFormatted = { road: "Road", oval: "Oval", dirt_oval: "Dirt Oval", dirt_road: "Dirt Road" };
 
   useEffect(() => {
-    retrieveFavoriteSeries();
-  });
+    loadFavoriteSeries();
+  }, []);
 
   /*  Function that will handle calling the backend to get the users favorite series
       Parameters: N/A
       Returns: N/A
   */
-  const retrieveFavoriteSeries = async () => {
-    getUserFavoritedSeries(user, favoriteSeries);
+  const loadFavoriteSeries = async () => {
+    const series = await getUserFavoritedSeries(user);
+    setUsersFavoriteSeries(new Map(series));
   };
 
   /* Function that will handle when a user selects a checkbox
@@ -33,7 +32,8 @@ export default function FavoriteSeries() {
       Returns: N/A
     */
   const handleSeriesSelected = (series, selected) => {
-    favoriteSeries.set(parseInt(series), selected);
+    let favorites = usersFavoriteSeries.set(parseInt(series), selected);
+    setUsersFavoriteSeries(new Map(favorites));
   };
 
   /* Function that will gather all the series data including id, name, and category
@@ -44,7 +44,9 @@ export default function FavoriteSeries() {
     let seriesList = [];
     Object.values(SeriesData).forEach((series) => {
       // Set the favorite series map to false
-      favoriteSeries.set(series.series_id, false);
+      if (usersFavoriteSeries.size == 0) {
+        usersFavoriteSeries.set(series.series_id, false);
+      }
 
       // Add the series data to the series list
       seriesList.push({ id: series.series_id, name: series.series_name, category: series.category });
@@ -61,7 +63,7 @@ export default function FavoriteSeries() {
     let seriesData = getSeriesData();
     let categorizedSeriesData = seriesData.map((series) => {
       if (series.category === category) {
-        return <FormControlLabel key={series.id} value={series.id} control={<Checkbox />} label={series.name} onChange={(e) => handleSeriesSelected(e.target.value, e.target.checked)} />;
+        return <FormControlLabel key={series.id} value={series.id} control={<Checkbox />} checked={usersFavoriteSeries.get(series.id) || false} label={series.name} onChange={(e) => handleSeriesSelected(e.target.value, e.target.checked)} />;
       }
     });
     return categorizedSeriesData;
@@ -73,7 +75,7 @@ export default function FavoriteSeries() {
     */
   const onFilterUpdate = async (event) => {
     try {
-      const body = { user: user, series: Object.fromEntries(favoriteSeries) };
+      const body = { user: user, series: Object.fromEntries(usersFavoriteSeries) };
       const response = await Axios.post("http://localhost:3001/users-content/update-favorite-series", body, { withCredentials: true }).then((response) => {
         if (response.status == 200) alert(response.data.message); // TODO: Make notification look nicer
       });
